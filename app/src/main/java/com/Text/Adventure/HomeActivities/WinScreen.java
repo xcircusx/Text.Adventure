78123e48fac32dcf82dd1a14fc7be36c8f28e41b
bejuli <julian.bermel@gmail.com>
2022-02-05 18:26:44 +0100

    Bugfixing

---
+++ app/src/main/java/com/Text/Adventure/HomeActivities/WinScreen.java
@@ -0,0 +1,16 @@
+package com.Text.Adventure.HomeActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.Text.Adventure.R;
+
+public class WinScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.win_screen);
+    }
+}
